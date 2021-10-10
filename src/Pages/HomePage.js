b67f4a445513a84ffe0d03a6c17fe86cf1efd3c4
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-10-10 22:18:59 +0530

    portfolio3

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,14 +3,12 @@ import styled from "styled-components";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import GithubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import Particle from "../Components/Particle";
+// import Particle from "../Components/Particle";
 import Typewriter from "../Components/Type";
 function HomePage() {
   return (
     <HomePageStyled>
-      <div className="particle-con">
-        <Particle />
-      </div>
+      <div className="particle-con">{/* <Particle /> */}</div>
       <div className="typography">
         <h1>
           Hi, I'm <span>Piyush Rana</span>

07a78bfba059f456edfa60079c8e349296d008c3
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-12-09 11:25:40 +0530

    add hackathon project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import css1 from "../img/portImages/nykaa.png";
 import css2 from "../img/portImages/trivago.png";
+import easy from "../img/portImages/easy.png";
 // import react1 from "../img/portImages/react_styled.png";
 // import img2 from "../img/portImages/maya-4.jpg";
 // import img3 from "../img/portImages/maya-3.jpg";
@@ -20,7 +21,7 @@ const portfolios = [
     image: css1,
     link1: "https://github.com/sarangKaliyath/Nykaa_Clone_V2.0",
     link2: "https://u3-ubk.herokuapp.com/",
-    title: "Nykaa V2.0",
+    title: "Nykaa Full-Stack",
     text: "Created with Ejs, Javascript, Css, Express, and MongoDb",
   },
   {
@@ -31,6 +32,15 @@ const portfolios = [
     link2: "https://trivagoclone.netlify.app/",
     title: "Trivago Clone",
     text: "Created with React",
+  },
+  {
+    id: 4,
+    category: "React",
+    image: easy,
+    link1: "https://github.com/piyushrana00456/Almost_human",
+    link2: "https://easy-delivery.netlify.app/",
+    title: "Easy Delivery",
+    text: "Created with React, MongoDb, Express",
   },
 ];
 
